1961b0394047f533831ecd470b78d5ac51366377
bruna <bruna-mac@brunas-MacBook-Pro.local>
2017-11-26 20:36:44 -0300

    Add test cases for GET /todos/:id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://brunabarbosa:brunaolib13@ds147681.mlab.com:47681/todo_app_brunabarbosa', {
+mongoose.connect(process.env.MONGODB_URI ||
+    'mongodb://brunabarbosa:brunaolib13@ds147681.mlab.com:47681/todo_app_brunabarbosa', {
     useMongoClient: true
 });
 
