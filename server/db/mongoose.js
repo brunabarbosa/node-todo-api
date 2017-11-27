cdf334d8a436c0c9aaed5ac3be451fd920b7aad8
bruna <bruna-mac@brunas-MacBook-Pro.local>
2017-11-27 15:03:18 -0300

    dbtest config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI ||
-    'mongodb://brunabarbosa:brunaolib13@ds147681.mlab.com:47681/todo_app_brunabarbosa', {
+mongoose.connect(process.env.MONGODB_URI, {
     useMongoClient: true
 });
 
